6d388321a87e3a8089412a4892b11670a34949a2
nandev <nandev.parkk@gmail.com>
2022-05-12 16:00:19 +0900

    show video list with title

---
+++ src/components/video_list/video_list.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VideoItem from "../video_item/video_item";
+
+export default function VideoList(props) {
+  return (
+    <ul>
+      {props.videos.map((video) => {
+        return <VideoItem key={video.id} video={video} />;
+      })}
+    </ul>
+  );
+}
