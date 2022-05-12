365308780246b880610646e8ae4ef1ba41969124
nandev <nandev.parkk@gmail.com>
2022-05-12 17:11:17 +0900

    make video item

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import styles from "./video_item.module.css";
 
-export default function VideoItem(props) {
-  return <h1>{props.video.snippet.title}</h1>;
+export default function VideoItem({ video: { snippet } }) {
+  return (
+    <li className={styles.container}>
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt={snippet.title}
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
+      </div>
+    </li>
+  );
 }
